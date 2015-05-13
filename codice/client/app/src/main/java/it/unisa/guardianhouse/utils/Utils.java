7a494b6ae74d3c969f0726755db21ff699d5bd88
cbranca <c.branca3@gmail.com>
2015-05-13 17:16:34 +0200

    Aggiunta classe Utils

---
+++ codice/client/app/src/main/java/it/unisa/guardianhouse/utils/Utils.java
@@ -0,0 +1,32 @@
+package it.unisa.guardianhouse.utils;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+public class Utils {
+
+    public static boolean hasConnection(Context c) {
+
+        ConnectivityManager cm = (ConnectivityManager) c.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo wifiNetwork = cm.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        if (wifiNetwork != null && wifiNetwork.isConnected()) {
+            return true;
+        }
+
+        NetworkInfo mobileNetwork = cm.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+        if (mobileNetwork != null && mobileNetwork.isConnected()) {
+            return true;
+        }
+
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        if (activeNetwork != null && activeNetwork.isConnected()) {
+            return true;
+        }
+
+        return false;
+
+    }
+
+}
