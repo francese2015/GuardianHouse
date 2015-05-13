a35f9c89d8b611caef4f657b9e54217636605c25
FabioCuccaro <thecomputerwhisper@hotmail.it>
2015-05-13 16:28:32 +0200

    Aggiornamento Activity Apartment

--- codice/client/app/src/main/java/it/unisa/guardianhouse/activities/ApartmentActivity.java
+++ codice/client/app/src/main/java/it/unisa/guardianhouse/activities/ApartmentActivity.java
@@ -181,7 +181,7 @@ public class ApartmentActivity extends ActionBarActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_account, menu);
+        getMenuInflater().inflate(R.menu.menu_apartment, menu);
         return true;
     }
 
