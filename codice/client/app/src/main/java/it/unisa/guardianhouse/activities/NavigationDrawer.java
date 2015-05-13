7ccaa86075f9d0f9e6eedad7383978ab904fbc3f
cbranca <c.branca3@gmail.com>
2015-05-13 19:55:57 +0200

    Aggiunto SearchFragment (da finire)

--- codice/client/app/src/main/java/it/unisa/guardianhouse/activities/NavigationDrawer.java
+++ codice/client/app/src/main/java/it/unisa/guardianhouse/activities/NavigationDrawer.java
@@ -4,17 +4,23 @@ import android.os.Bundle;
 
 import it.neokree.materialnavigationdrawer.MaterialNavigationDrawer;
 import it.neokree.materialnavigationdrawer.elements.MaterialSection;
+import it.neokree.materialnavigationdrawer.elements.listeners.MaterialSectionListener;
 import it.unisa.guardianhouse.fragments.BlankFragment;
+import it.unisa.guardianhouse.fragments.SearchFragment;
 
-public class NavigationDrawer extends MaterialNavigationDrawer {
+public class NavigationDrawer extends MaterialNavigationDrawer implements MaterialSectionListener {
+
+    private MaterialSection blankSection;
+    private MaterialSection search;
 
     @Override
     public void init(Bundle savedInstanceState) {
-        MaterialSection section = newSection("Section 1", new BlankFragment());
 
-        this.addAccountSection(section);
-        this.addSection(section);
-        this.addBottomSection(section);
+        blankSection = newSection("Section 1", new BlankFragment());
+        search = newSection("Ricerca", new SearchFragment());
+
+        this.addSection(this.blankSection);
+        this.addSection(this.search);
 
     }
 }
