212d87033b9e79bd4727d5bd928949a061dafa8f
cbranca <c.branca3@gmail.com>
2015-06-15 13:23:43 +0200

    Parziale sistemazione dell'inserimento appartamento

--- codice/client/app/src/main/java/it/unisa/guardianhouse/fragments/ApartmentEntryReviewFragment.java
+++ codice/client/app/src/main/java/it/unisa/guardianhouse/fragments/ApartmentEntryReviewFragment.java
@@ -4,6 +4,7 @@ package it.unisa.guardianhouse.fragments;
 import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,10 +16,8 @@ import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
 import com.gc.materialdesign.views.ButtonRectangle;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -29,13 +28,13 @@ import it.neokree.materialnavigationdrawer.MaterialNavigationDrawer;
 import it.unisa.guardianhouse.AppController;
 import it.unisa.guardianhouse.R;
 import it.unisa.guardianhouse.config.Config;
-import it.unisa.guardianhouse.models.Apartment;
+import it.unisa.guardianhouse.helpers.SessionManager;
+import it.unisa.guardianhouse.utils.MyJsonObjectRequest;
 
-/**
- * A simple {@link Fragment} subclass.
- */
+
 public class ApartmentEntryReviewFragment extends Fragment {
 
+    private static final String TAG = ApartmentEntryFragment.class.getSimpleName();
     private ButtonRectangle btnToSave;
     private String description;
     private double furniture_quality;
@@ -46,7 +45,6 @@ public class ApartmentEntryReviewFragment extends Fragment {
     private double neighbours;
     private double rating;
     private double house_conditions;
-    Bundle bundle;
     private EditText inputReview;
     private RatingBar rateFurniture;
     private RatingBar rateThermic;
@@ -58,9 +56,8 @@ public class ApartmentEntryReviewFragment extends Fragment {
     private RatingBar rateConditions;
     private HashMap<String, String> params;
     private String url;
-    private String aptId;
-    private ProgressDialog pDialog;
-    private String civic;
+    SessionManager session;
+    Bundle bundle;
 
 
     public ApartmentEntryReviewFragment() {
@@ -71,11 +68,13 @@ public class ApartmentEntryReviewFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
-
         View view = inflater.inflate(R.layout.fragment_3_review, container, false);
 
-        btnToSave = (ButtonRectangle) view.findViewById(R.id.button_to_save);
+        session = new SessionManager(getActivity());
+
+        bundle = getArguments();
+
+
         inputReview = (EditText) view.findViewById(R.id.txtReview);
         rateFurniture = (RatingBar) view.findViewById(R.id.ratingBar1);
         rateThermic = (RatingBar) view.findViewById(R.id.ratingBar2);
@@ -86,162 +85,141 @@ public class ApartmentEntryReviewFragment extends Fragment {
         rateExperience = (RatingBar) view.findViewById(R.id.ratingBar7);
         rateConditions = (RatingBar) view.findViewById(R.id.ratingBar8);
 
-        pDialog = new ProgressDialog(getActivity());
-        pDialog.setCancelable(false);
-
+        btnToSave = (ButtonRectangle) view.findViewById(R.id.button_to_save);
 
         btnToSave.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View view) {
 
-                url = Config.APARTMENTS_URL  ;
+                url = Config.APARTMENTS_URL;
 
                 description = inputReview.getText().toString();
-                furniture_quality = rateFurniture.getNumStars();
-                thermic_capacity = rateThermic.getNumStars();
-                landlord_honesty = rateLandlord.getNumStars();
-                security_level = rateSecurity.getNumStars();
-                bus_connection = rateBusNear.getNumStars();
-                neighbours = rateNeighbours.getNumStars();
-                rating = rateExperience.getNumStars();
-                house_conditions = rateConditions.getNumStars();
+                furniture_quality = rateFurniture.getRating();
+                thermic_capacity = rateThermic.getRating();
+                landlord_honesty = rateLandlord.getRating();
+                security_level = rateSecurity.getRating();
+                bus_connection = rateBusNear.getRating();
+                neighbours = rateNeighbours.getRating();
+                rating = rateExperience.getRating();
+                house_conditions = rateConditions.getRating();
 
                 if (!description.isEmpty()) {
 
-                    bundle = getArguments();
-
-                    bundle.putString("myReview", description);
-                    bundle.putDouble("myFurniture", furniture_quality);
-                    bundle.putDouble("myThermic", thermic_capacity);
-                    bundle.putDouble("myLandlord", landlord_honesty);
-                    bundle.putDouble("mySecurity", security_level);
-                    bundle.putDouble("myBusNear", bus_connection);
-                    bundle.putDouble("myNeighbours", neighbours);
-                    bundle.putDouble("myExperience", rating);
-                    bundle.putDouble("myConditions", house_conditions);
-
                     params = new HashMap<>();
-                    //apartmentEntryReview
-                    params.put("myReview", description);
-                    params.put("myFurniture", String.valueOf(bundle.getDouble("furniture_quality")));
-                    params.put("myThermic", String.valueOf(bundle.getDouble("thermic_capacity")));
-                    params.put("myLandlord", String.valueOf(bundle.getDouble("landlord_honesty")));
-                    params.put("mySecurity", String.valueOf(bundle.getDouble("security_level")));
-                    params.put("myBusNear", String.valueOf(bundle.getDouble("bus_connection")));
-                    params.put("myNeighbours", String.valueOf(bundle.getDouble("neighbours")));
-                    params.put("myExperience", String.valueOf(bundle.getDouble("rating")));
-                    params.put("myConditions", String.valueOf(bundle.getDouble("house_conditions")));
 
                     //apartmentEntry
-
-                    params.put("myCivic", bundle.getString("street_number"));
-                    params.put("myInter", bundle.getString("intern_id"));
-                    params.put("myConditions", bundle.getString("status"));
-                    params.put("myRoad", bundle.getString("route"));
+                    params.put("myTitle", bundle.getString("name"));
                     params.put("myDescript", bundle.getString("description"));
                     params.put("myMeters", bundle.getString("mq"));
                     params.put("myCarspot", bundle.getString("car_place"));
                     params.put("myContract", bundle.getString("contract_time"));
-                    params.put("myTitle", bundle.getString("name"));
-                    params.put("myLocality", bundle.getString("locality"));
+                    // cost
+                    params.put("myConditions", bundle.getString("status"));
 
+                    params.put("myCivic", bundle.getString("street_number"));
+                    params.put("myInter", bundle.getString("intern_id"));
+                    params.put("myLocality", bundle.getString("locality"));
+                    params.put("myRoad", bundle.getString("route"));
+                    //administrative area level 1
+                    //administrative area level 2
+                    //postal code
+                    //country
+
+                    params.put("myLatitude", bundle.getString("latitude"));
+                    params.put("myLongitude", bundle.getString("longitude"));
 
                     //apartmentEntryRoom
-                    params.put("myGuest", bundle.getString("guest"));
-                    params.put("myRoom", bundle.getString("room"));
+                    //params.put("myGuest", bundle.getString("guest"));
+                    //params.put("myRoom", bundle.getString("room"));
                     params.put("myFree", bundle.getString("free_rooms"));
-                    params.put("myBed", bundle.getString("bed"));
-                    params.put("myWc", bundle.getString("wc"));
+                    //params.put("myBed", bundle.getString("bed"));
+                    //params.put("myWc", bundle.getString("wc"));
 
+                    //apartmentEntryReview
+                    params.put("myConditions", String.valueOf(house_conditions));
+                    params.put("myThermic", String.valueOf(thermic_capacity));
+                    params.put("myLandlord", String.valueOf(landlord_honesty));
+                    params.put("mySecurity", String.valueOf(security_level));
+                    params.put("myBusNear", String.valueOf(bus_connection));
+                    params.put("myNeighbours", String.valueOf(neighbours));
+                    //distance_cc ?
+                    params.put("myFurniture", String.valueOf(furniture_quality));
+                    params.put("myReview", description);
+                    params.put("myExperience", String.valueOf(rating));
+                    params.put("user_id", session.getUserId());
+                    params.put("username", session.getUsername());
 
-                    saveApartment();
-
-
-
+                    makeRequest();
 
                 } else {
-
-
                     Toast.makeText(getActivity(),
                             "Riempi la Recensione!", Toast.LENGTH_LONG)
                             .show();
-
-
                 }
             }
-
-            public void saveApartment() {
-                JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.POST, url,
-                        (String) null, new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        try {
-                            JSONArray apartmentArray = response.getJSONArray("apartments");
-                            for (int i = 0; i < apartmentArray.length(); i++) {
-                                JSONObject singleApartment = apartmentArray.getJSONObject(i);
-                                Apartment apartment = new Apartment();
-
-                                apartment.setId(singleApartment.getJSONObject("_id").getString("$id"));
-                                apartment.setLocality(singleApartment.getJSONObject("address").getString("locality"));
-                                apartment.setInternId(singleApartment.getJSONObject("address").getString("intern_id"));
-                                apartment.setStreetNumber(singleApartment.getJSONObject("address").getString("street_number"));
-                                apartment.setRoute(singleApartment.getJSONObject("address").getString("route"));
-                                apartment.setName(singleApartment.getJSONObject("details").getString("name"));
-                                apartment.setMq(singleApartment.getJSONObject("details").getDouble("mq"));
-                                apartment.setCarPlace(singleApartment.getJSONObject("details").getDouble("car_place"));
-                                apartment.setFreeRooms(singleApartment.getJSONObject("details").getDouble("free_rooms"));
-                                apartment.setCost(singleApartment.getJSONObject("details").getDouble("cost"));
-                                apartment.setDescription(singleApartment.getJSONObject("details").getString("description"));
-                                apartment.setStatus(singleApartment.getJSONObject("details").getString("status"));
-                                apartment.add(apartment);
-
-
-                                Toast.makeText(getActivity(),
-                                        "Inserimento Avvenuto Con Successo", Toast.LENGTH_LONG)
-                                        .show();
-
-                                HomeFragment homeFragment = new HomeFragment();
-                                ((MaterialNavigationDrawer) getActivity()).setFragment(homeFragment, "Home");
-                            }
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-
-
-                    }
-                }, new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-
-                    }
-                }) {
-                    @Override
-                    public Map<String, String> getHeaders() throws AuthFailureError {
-                        HashMap<String, String> headers = new HashMap<String, String>();
-                        headers.put("Content-Type", "application/x-www-form-urlencoded");
-                        return headers;
-                    }
-                };
-
-                AppController.getInstance().addToRequestQueue(jsonObjReq);
-            }
-
-            private void showDialog() {
-                if (!pDialog.isShowing())
-                    pDialog.show();
-            }
-
-            private void hideDialog() {
-                if (pDialog.isShowing())
-                    pDialog.dismiss();
-            }
-
-
 
 
         });
         return view;
     }
 
+
+    private void makeRequest() {
+        // Tag used to cancel the request
+        String tag_jObj_req = "req_insert_apartment";
+
+        MyJsonObjectRequest strReq = new MyJsonObjectRequest(Request.Method.POST,
+                url, params, new Response.Listener<JSONObject>() {
+
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.d(TAG, "Insert Review Response: " + response.toString());
+
+
+                try {
+                    Boolean error = response.getBoolean("error");
+
+                    // Check for error node in json
+                    if (!error) {
+                        Toast.makeText(getActivity(),
+                                "Appartamento inserito!", Toast.LENGTH_LONG).show();
+
+                        HomeFragment homeFragment = new HomeFragment();
+                        ((MaterialNavigationDrawer) getActivity()).setFragment(homeFragment, "Home");
+
+                    } else {
+                        // Get the error message
+                        //String errorMsg = response.getString("error_msg");
+                        //Toast.makeText(getActivity(),
+                        //        errorMsg, Toast.LENGTH_LONG).show();
+                    }
+                } catch (JSONException e) {
+                    // JSON error
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(TAG, "Login Error: " + error.getMessage());
+                Toast.makeText(getActivity(),
+                        error.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        }) {
+
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<String, String>();
+                headers.put("Content-Type", "application/x-www-form-urlencoded");
+                headers.put("Authorization", session.getApiKey());
+                return headers;
+            }
+
+        };
+
+        // Adding request to request queue
+        AppController.getInstance().addToRequestQueue(strReq, tag_jObj_req);
     }
 
+}
